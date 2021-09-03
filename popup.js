db6d4b071af75b57a906cf8263612781a79cb935
Maga <magalisantiago@hotmail.com>
2021-09-03 14:08:36 -0300

    Boton enviar del formulario sponsors

--- popup.js
+++ popup.js
@@ -8,8 +8,27 @@ btnAbrirPopup.addEventListener('click', function(){
 	popup.classList.add('active');
 });
 
+
 btnCerrarPopup.addEventListener('click', function(e){
 	e.preventDefault();
 	overlay.classList.remove('active');
 	popup.classList.remove('active');
 });
+
+/*Boton 2 de registrarme
+let btnAbrirPopup2 = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay-2'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+}); */
